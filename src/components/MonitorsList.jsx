1dfc6befe2519ddf059c69aa7360bbfb6e605aca
7-18 <kevinbrianbr@gmail.com>
2022-07-03 22:26:54 -0500

    monitoring app

--- src/components/MonitorsList.jsx
+++ src/components/MonitorsList.jsx
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
+import { avatars } from "../data/avatar";
 import { getMonitorsAsync } from "../redux/actions/actionMonitors";
+import { CardDiv, CardStyled } from "../styles/GlobalStyles";
+
+const randomAvatar = () => {
+  const random = Math.floor(Math.random() * avatars.length);
+  return avatars[random];
+};
 
 export const MonitorsList = () => {
   const dispatch = useDispatch();
@@ -11,34 +19,45 @@ export const MonitorsList = () => {
   }, []);
 
   return (
-    <div>
-      <div>Lista de Monitores</div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Programa Académico</th>
-            <th>Semestre</th>
-            <th>Email</th>
-            <th>Teléfono</th>
-            <th>Cédula</th>
-          </tr>
-        </thead>
-        <tbody>
+    <CardDiv>
+      <Container>
+        <Row xs={1} md={3} className="g-4">
           {monitors.map((monitor) => (
-            <tr key={monitor.id}>
-              <td>{monitor.name}</td>
-              <td>{monitor.lastName}</td>
-              <td>{monitor.academic_program}</td>
-              <td>{monitor.semester}</td>
-              <td>{monitor.email}</td>
-              <td>{monitor.phone}</td>
-              <td>{monitor.id}</td>
-            </tr>
+            <Col key={monitor.id}>
+              <CardStyled>
+                <img src={randomAvatar()} />
+                <Card.Body>
+                  <Card.Title className="text-center border-2 border-bottom">
+                    {monitor.name} {monitor.lastName}
+                  </Card.Title>
+                  <ul>
+                    <li>
+                      <span>Email:</span>
+                      {monitor.email}
+                    </li>
+                    <li>
+                      <span>Teléfono:</span>
+                      {monitor.phone}
+                    </li>
+                    <li>
+                      <span>Programa académico:</span>
+                      {monitor.academic_program}
+                    </li>
+                    <li>
+                      <span>Semestre:</span>
+                      {monitor.semester}
+                    </li>
+                    <li>
+                      <span>Cédula:</span>
+                      {monitor.id}
+                    </li>
+                  </ul>
+                </Card.Body>
+              </CardStyled>
+            </Col>
           ))}
-        </tbody>
-      </table>
-    </div>
+        </Row>
+      </Container>
+    </CardDiv>
   );
 };
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,3 +1,4 @@
+import { Card } from "react-bootstrap";
 import styled from "styled-components";
 
 export const SpanLogo = styled.span`
@@ -8,5 +9,38 @@ export const SpanLogo = styled.span`
   & > a {
     text-decoration: none;
     color: #000;
+  }
+`;
+
+export const CardDiv = styled.div`
+  padding: 30px;
+`;
+
+export const CardStyled = styled(Card)`
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.3);
+  border: 1px solid transparent;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+
+  & > img {
+    width: 100px;
+    height: 100px;
+  }
+
+  & > div > ul {
+    list-style: circle;
+    margin-top: 20px;
+
+    & > li {
+      color: #000000;
+      font-size: 12px;
+      margin-bottom: 5px;
+
+      & > span {
+        margin-right: 5px;
+        font-weight: bold;
+      }
   }
 `;
